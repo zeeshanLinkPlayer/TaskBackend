36c81a901f5845f23a70075b6f75c92f72a3711b
zeeshanLinkPlayer <linkplayer23577@gmail.com>
2025-03-18 12:10:00 +0500

    remove slash

--- server.js
+++ server.js
@@ -27,9 +27,9 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 // API Routes
-app.use('/api/auth', require('./routes/authRoutes'));
-app.use('/api/tasks', require('./routes/taskRoutes'));
-app.use('/api/users', require('./routes/userRoutes'));
+app.use('api/auth', require('./routes/authRoutes'));
+app.use('api/tasks', require('./routes/taskRoutes'));
+app.use('api/users', require('./routes/userRoutes'));
 
 // Serve static assets in production
 // if (process.env.NODE_ENV === 'production') {
